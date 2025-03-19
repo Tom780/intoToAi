a98b1207669b8b678c3e5e84aaf56e5c1570172f
tom780 <you@example.com>
2025-03-19 11:33:00 +0000

    import base manhatten stuff

--- robot.py
+++ robot.py
@@ -4,6 +4,8 @@ from agent import Agent
 import random
 import environment
 import utils
+import heapq
+
 class Robot(Agent):
 
     def __init__(self, position: tuple[int, int],map_size = (30,17)):
@@ -13,7 +15,7 @@ class Robot(Agent):
         self.current_location = position
         self.map_size = map_size
         self.map = [["?" for _ in range(map_size[1])] for _ in range(map_size[0])]
-
+        self.docking_station_location = None
 
     def decide(self, percept: dict[tuple[int, int], ...]):
         valid_options = []
@@ -28,6 +30,9 @@ class Robot(Agent):
         forward_position = movement_directions.get(self.orientation)
 
         docking_station = [pos for pos, obj in percept.items() if utils.is_docking_station(obj)]
+        if docking_station:
+            self.docking_station_location = docking_station[0]
+
         if docking_station and self.battery_life < 100:
             chosen_move = ("charge",self.position[0])
         else:
@@ -81,9 +86,66 @@ class Robot(Agent):
             if environment.move_to(self.position, to):
                 self.position = to
                 self.battery_life = self.battery_life - 1
+                self.map[self.position[0]][self.position[1]] = " "
                 print(f"Robot moved to {self.position}")
                 print(f"The battery life is {self.battery_life}")
 
+        # MANHATTAN DISTANCE FUNCTIONS
+
+    def calc_path(self, start, goal, avoid):
+        p_queue = []
+        heapq.heappush(p_queue, (0, start))
+
+        directions = {
+            "right": (0, 1),
+            "left": (0, -1),
+            "up": (-1, 0),
+            "down": (1, 0)
+        }
+        predecessors = {start: None}
+        g_values = {start: 0}
+
+        while len(p_queue) != 0:
+            current_cell = heapq.heappop(p_queue)[1]
+            if current_cell == goal:
+                return self.get_path(predecessors, start, goal)
+            for direction in ["up", "right", "down", "left"]:
+                row_offset, col_offset = directions[direction]
+                neighbour = (current_cell[0] + row_offset, current_cell[1] + col_offset)
+
+                if self.viable_move(neighbour[0], neighbour[1], avoid) and neighbour not in g_values:
+                    cost = g_values[current_cell] + 1
+                    g_values[neighbour] = cost
+                    f_value = cost + self.calc_distance(goal, neighbour)
+                    heapq.heappush(p_queue, (f_value, neighbour))
+                    predecessors[neighbour] = current_cell
+
+    def get_path(self, predecessors, start, goal):
+        current = goal
+        path = []
+        while current != start:
+            path.append(current)
+            current = predecessors[current]
+        path.append(start)
+        path.reverse()
+        return path
+
+    def viable_move(self, x, y, types):
+        # You will need to do this one
+        # Do not move in to a cell containing an obstacle (represented by 'x')
+        # Do not move in to a cell containing a flame
+        # Do not move in to a cell containing a water station
+        # Do not move in to a cell containing a robot.
+        # In fact, the only valid cells are blank ones
+        # Also, do not go out of bounds.
+        pass
+
+    def calc_distance(self, point1: tuple[int, int], point2: tuple[int, int]):
+        x1, y1 = point1
+        x2, y2 = point2
+        return abs(x1 - x2) + abs(y1 - y2)
+
+    # END OF MANHATTAN DISTANCE FUNCTIONS
 
     def __str__(self):
         return self.orientation
